5e0079504567091f69d39500b2e201398c6285bc
deijahmonay <deijah@shack15.com>
2024-12-06 16:51:22 -0800

    import use effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar.jsx'
 

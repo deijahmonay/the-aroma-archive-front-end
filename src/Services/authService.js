754ca9d65d2b555030b10013769a667c17c1cc31
deijahmonay <deijah@shack15.com>
2024-12-09 09:26:51 -0800

    add error handling for api signup fuction + error handling

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -2,8 +2,22 @@ const BACKEND_URL = 'http://localhost:3000'
 
 const signup = async (formData) => {
   try {
+    const resn= await fetch(`${BACKEND_URL}/users/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(formData),
+    })
+    const json = await resn.json()
+    if(json.err) {
+      throw new Error(json.err)
+    }
 
   }catch(err) {
-    
+    console.log(err)
+    throw  err
   }
+}
+
+export {
+  signup,
 }

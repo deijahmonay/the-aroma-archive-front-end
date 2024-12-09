ea8e2dbd0b9b2c622bea92fec1e0dc30b1faaf5f
deijahmonay <deijah@shack15.com>
2024-12-08 17:18:46 -0800

    add visitor message

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,9 @@
 const Landing = () => {
   return (
     <main>
-
+      <h1>Welcome to the landing page for visitors</h1>
+      <h3>Sign up for a new account to unlock access to your exclusive Aroma Archive dashboard, 
+        where your perfume journey awaits!</h3>
     </main>
   )
 }
